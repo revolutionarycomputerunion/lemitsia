f61f95942e269670949ae4fc581e40cdbfffe29a
trcu <waresofaware@gmail.com>
2026-05-31 16:25:47 +0500

    Create admins.lua

---
+++ admins.lua
@@ -0,0 +1,62 @@
+	settings.Ranks = {
+		["Moderators"] = {
+			Level = 100;
+			Users = {
+				"Group:35837815:13",
+				"Group:35837815:16",
+				"Group:35837815:14",
+				"Group:918140049:20",
+				"Group:918140049:21",
+				"Group:35468180:94",
+				"Group:35468180:95",
+				"Group:36017035:105",
+				"Group:36017035:110",
+				"Group:36017035:115",
+	
+			};
+		};
+
+		["Administrators"] = {
+			Level = 200;
+			Users = {
+			"Group:35837815:248",
+			"Group:35837815:247",
+			"Group:35837815:20",
+			"Group:918140049:30",
+			"Group:918140049:31",
+			"Group:918140049:240",
+			"Group:918140049:241",
+			"Group:918140049:253",
+			"Group:918140049:254",
+			"Group:918140049:255",
+			"Group:36017035:155",
+			"Group:36017035:160",
+			"Group:36017035:165",
+			};
+		};
+
+		["High Administrator"] = {
+			Level = 300;
+			Users = {
+			"Group:918140049:240",
+			"Group:918140049:241",
+			"Group:36017035:205",
+			"Group:36017035:210",
+			"Group:36017035:215",
+				
+			};
+		};
+		["Chief Administrator"] = {
+		Level = 400;
+		 	Users = {
+				"Group:35837815:250",
+				"Group:35837815:249",
+				"Group:36017035:220",
+			};  
+		};
+		["Larp DEVS"] = {
+		Level = 300;
+		Users = {
+			"Group:35837815:246",--new trcu devs
+		};  
+	};
